cb7a56e80a5274d53648fb7a02c0b0283245ef69
Wim <you@example.com>
2019-02-10 10:46:28 +0100

    adding houses

---
+++ ithoapp/src/app/house.ts
@@ -0,0 +1,4 @@
+export class House {
+    name: string;
+    ip: string;
+}
--- ithoapp/src/app/houses-list/houses-list.component.ts
+++ ithoapp/src/app/houses-list/houses-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { House } from '../house';
 
 @Component({
   selector: 'app-houses-list',
