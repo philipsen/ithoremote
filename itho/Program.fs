9f27b1ffef4043cd94c5ed89824a0b3f56c67a50
Wim <you@example.com>
2019-02-26 21:19:23 +0100

    try1

--- itho/Program.fs
+++ itho/Program.fs
@@ -1,8 +1,36 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+﻿  open Giraffe
+  open Giraffe.GiraffeViewEngine
+  open Microsoft.AspNetCore.Builder
+  open Microsoft.Extensions.DependencyInjection
+  open Microsoft.AspNetCore.Hosting
+  //open Microsoft.AspNetCore.Mvc.UrlHelperExtensions
+  open System
+  
+  let index = 
+      html [] [
+          head [] [
+              title [] [ str "Giraffe!" ]
+          ]
+          body [] [
+              h1 [] [ str "Hello!" ]
+              p [] [ str "A test of Giraffe and .NET Core"]
+          ]
+      ]
+  
+  let webApp =
+      choose [ route "/" >=> (index |> renderHtmlDocument |> htmlString) ]
+  
+  let configureApp (app : IApplicationBuilder) =
+      app.UseGiraffe(webApp)
+               
+  let configureServices (services : IServiceCollection) =
+      services.AddGiraffe() |> ignore
+  
+  //[]
+  let main _ =
+      WebHostBuilder()
+          .UseKestrel()
+          .Configure(Action<IApplicationBuilder>  configureApp)
+          .ConfigureServices(configureServices)
+          .Build()
+          .Run()
