d9ba2119f01b1aaa0a95fec0ce548230830fb40c
Wim <you@example.com>
2020-03-21 22:34:47 +0100

    refactor to use descriminating union

--- IthoRemoteApp/src/IthoRemoteApp/Mqtt.fs
+++ IthoRemoteApp/src/IthoRemoteApp/Mqtt.fs
@@ -6,32 +6,54 @@ open uPLibrary.Networking.M2Mqtt.Messages
 open Log
 open Microsoft.Extensions.Configuration
 
-let node = MqttClient(brokerHostName="167.99.32.103")
+let (|ControlBox|HandheldRemote|TransmitRequest|Fanspeed|Unknown|) (topic: string) = 
+    match topic.Split("/") with
+    | [|"itho"; transponder; "received"; "allcb"|] -> ControlBox transponder
+    | [|"itho"; transponder; "received"; "allremotes"|] -> HandheldRemote transponder
+    | [|"itho"; transponder; "received"; "handheld"|]
+    | [|"itho"; transponder; "command"; "transmit"|] -> TransmitRequest transponder
+    | [|"itho"; house; "received"; "fanspeed"|] -> Fanspeed house
+    | _ -> Unknown
 
 let msgReceived (mqttEvent:MqttMsgPublishEventArgs) =
     let message = Encoding.ASCII.GetString mqttEvent.Message
     sprintf "mqtt: received %s -> %s" mqttEvent.Topic message |> Information
 
-    match mqttEvent.Topic.Split("/") with
-    | [|"itho"; house; "received"; "allcb"|] -> 
-        Domain.eventFromControlBoxPacket house message
-
-    | [|"itho"; house; "received"; "allremotes"|] -> 
-        Domain.eventFromRemote house message
-
-    | [|"itho"; house; "received"; "handheld"|]
-    | [|"itho"; house; "command"; "transmit"|] ->
+    match mqttEvent.Topic with
+    | ControlBox transponder -> Domain.eventFromControlBoxPacket transponder message
+    | HandheldRemote transponder -> Domain.eventFromRemote transponder message
+    | TransmitRequest transponder ->
         match message.Split("/") with
         | [| remote ; command |] ->
-            Domain.createIthoTransmitRequestEvents house remote command
+            Domain.createIthoTransmitRequestEvents transponder remote command
         | _ -> printf "unknown command ignored %s\n" message
-
-    | [|"itho"; house; "received"; "fanspeed"|] -> 
-        Domain.HouseAggregate.createIthoFanSpeedEvent house message
-
+    | Fanspeed house -> Domain.HouseAggregate.createIthoFanSpeedEvent house message
     | _ -> ()
 
+    // match mqttEvent.Topic.Split("/") with
+    // | [|"itho"; house; "received"; "allcb"|] -> 
+    //     Domain.eventFromControlBoxPacket house message
+
+    // | [|"itho"; house; "received"; "allremotes"|] -> 
+    //     Domain.eventFromRemote house message
+
+    // | [|"itho"; house; "received"; "handheld"|]
+    // | [|"itho"; house; "command"; "transmit"|] ->
+    //     match message.Split("/") with
+    //     | [| remote ; command |] ->
+    //         Domain.createIthoTransmitRequestEvents house remote command
+    //     | _ -> printf "unknown command ignored %s\n" message
+
+    // | [|"itho"; house; "received"; "fanspeed"|] -> 
+    //     Domain.HouseAggregate.createIthoFanSpeedEvent house message
+
+    // | _ -> ()
+
+
+let mutable node = null
+
 let InitializeMqtt(settings: IConfiguration) =
+    node <- MqttClient(settings.["MqttHost"])
     node.Connect("fs_rec", settings.["MqttUser"], settings.["MqttPassword"]) |> ignore
     sprintf "mtqq connection: %A" node.IsConnected |> Information
     let topics = [| "#" |]
